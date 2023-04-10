9f6330c25287e978809204031eae4da329419587
Olloshukur6092 <matnazarovolloshukur3107223@gmail.com>
2023-04-10 18:17:15 +0500

    ..

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
+    @vite('resources/css/app.css')
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
-<body>
-    <h1>Salom Home Page</h1>
+
+<body class="bg-blue-500">
+    {{-- header --}}
+    @include('layouts.header')
 </body>
+
 </html>
