4a2eda258b2ad7b50f64f3394e3e47e38a1e1612
Olloshukur6092 <matnazarovolloshukr7223@gmail.com>
2023-05-04 13:13:53 +0500

    Viloyatlar accordioni bitdi

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,7 +7,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
+                <li class="nav-item ">
                     <a class="nav-link text-uppercase" href="{{ route('client.home') }}">Bosh sahifa</a>
                 </li>
                 <li class="nav-item">
