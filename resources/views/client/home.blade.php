e29f5952899573b67707e8d95452b2a4cdaa333c
Olloshukur6092 <matnazarovolloshukr7223@gmail.com>
2023-05-06 10:34:20 +0500

    -

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -67,36 +67,6 @@
                     </div>
                 </div>
             </div>
-
-            {{-- ommaboplari --}}
-            {{-- <div class="home-popular my-5">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="popular-text">
-                            <p class="d-flex align-items-center"><i
-                                    class="fa-solid fa-star fa-2x me-3 mb-1"></i><strong>Ommabop tovarlar</strong></p>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="row">
-                    @foreach (['0' => 'Buxsi', '1' => 'Futbolka', '2' => 'Futbolka classic', '3' => 'Futbolka sport'] as $key => $value)
-                        <div class="col-lg-3 col-md-4 col-sm-6">
-                            <div class="card card-body border-0 shadow-lg p-0 position-relative mb-3">
-                                <div class="card-img-block">
-                                    <img src="{{ asset('images/cr' . $key . '.jpg') }}" alt="Image Not Found"
-                                        class="img-fluid" />
-                                </div>
-                                <div class="card-img-text position-absolute">
-                                    <p>{{$value}}</p>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-
-                </div>
-            </div>   --}}
-            {{-- Eng xitlari --}}
             <div class="xit-products shadow rounded-5 p-4 my-4">
                 <div class="text-slider-icon d-flex justify-content-between align-items-center p-2">
                     <div class="text-product">
@@ -132,94 +102,6 @@
                                     </div>
                                 </div>
                             @endforeach
-                            {{-- <div class="col-md-3 me-3">
-                                <div class="card card-body p-0 border-0">
-                                    <div class="product-img">
-                                        <img src="{{ asset('images/shoes/sh2.png') }}" alt="Product Image"
-                                            class="img-fluid" />
-                                    </div>
-                                    <div class="product-name mt-2">
-                                        <p>Kovush</p>
-                                    </div>
-                                    <div class="product-price">
-                                        <p>200 000 so'm</p>
-                                    </div>
-                                    <div class="add-product">
-                                        <button> <i class="fa-solid fa-basket-shopping"></i> &nbsp;Savatga qo'shish</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3 me-3">
-                                <div class="card card-body p-0 border-0">
-                                    <div class="product-img">
-                                        <img src="{{ asset('images/shoes/sh3.png') }}" alt="Product Image"
-                                            class="img-fluid" />
-                                    </div>
-                                    <div class="product-name mt-2">
-                                        <p>Kovush</p>
-                                    </div>
-                                    <div class="product-price">
-                                        <p>200 000 so'm</p>
-                                    </div>
-                                    <div class="add-product">
-                                        <button> <i class="fa-solid fa-basket-shopping"></i> &nbsp;Savatga qo'shish</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3 me-3">
-                                <div class="card card-body p-0 border-0">
-                                    <div class="product-img">
-                                        <img src="{{ asset('images/shoes/sh4.png') }}" alt="Product Image"
-                                            class="img-fluid" />
-                                    </div>
-                                    <div class="product-name mt-2">
-                                        <p>Kovush</p>
-                                    </div>
-                                    <div class="product-price">
-                                        <p>200 000 so'm</p>
-                                    </div>
-                                    <div class="add-product">
-                                        <button> <i class="fa-solid fa-basket-shopping"></i> &nbsp;Savatga
-                                            qo'shish</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3 me-3">
-                                <div class="card card-body p-0 border-0">
-                                    <div class="product-img">
-                                        <img src="{{ asset('images/shoes/sh4.png') }}" alt="Product Image"
-                                            class="img-fluid" />
-                                    </div>
-                                    <div class="product-name mt-2">
-                                        <p>Kovush</p>
-                                    </div>
-                                    <div class="product-price">
-                                        <p>200 000 so'm</p>
-                                    </div>
-                                    <div class="add-product">
-                                        <button> <i class="fa-solid fa-basket-shopping"></i> &nbsp;Savatga
-                                            qo'shish</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3 me-3">
-                                <div class="card card-body p-0 border-0">
-                                    <div class="product-img">
-                                        <img src="{{ asset('images/shoes/sh1.png') }}" alt="Product Image"
-                                            class="img-fluid" />
-                                    </div>
-                                    <div class="product-name mt-2">
-                                        <p>Kovush</p>
-                                    </div>
-                                    <div class="product-price">
-                                        <p>200 000 so'm</p>
-                                    </div>
-                                    <div class="add-product">
-                                        <button> <i class="fa-solid fa-basket-shopping"></i> &nbsp;Savatga
-                                            qo'shish</button>
-                                    </div>
-                                </div>
-                            </div> --}}
                         </div>
                     </div>
                 </div>
